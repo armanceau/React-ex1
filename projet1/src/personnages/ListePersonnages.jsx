a41ebefe5527d1e33cd9370380b780f079567cb3
armanceau <102350268+armanceau@users.noreply.github.com>
2024-06-07 09:14:35 +0200

    init annuaire

--- projet1/src/personnages/ListePersonnages.jsx
+++ projet1/src/personnages/ListePersonnages.jsx
@@ -6,7 +6,7 @@ export const ListePersonnage = ({liste}) => {
             {liste.map((value, index) => {
                 return(
                     <div>
-                        <FichePersonnage  key={index} nom={value.nom} prenom={value.prenom} image={value.image} />
+                        <FichePersonnage key={index} nom={value.nom} prenom={value.prenom} image={value.image} />
                     </div>
                 )
             })}
